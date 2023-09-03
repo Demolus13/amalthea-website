4e0761b56ea77c7e948bd2bb291b29aa27408a70
Romit Mohane <69901221+Reckadon@users.noreply.github.com>
2023-09-03 16:22:14 +0530

    prettify

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import './App.css';
-import Home from './components/Home'
-import Navbar from './components/Navbar';
-import Events from './components/Events';
-import Conclave from './components/Conclave';
-import Symposium from './components/Symposium';
-import TechExpo from './components/TechExpo';
-import Sponsors from './components/Sponsors';
-import {Routes, Route} from 'react-router-dom';
+import "./App.css";
+import Home from "./components/Home";
+import Navbar from "./components/Navbar";
+import Events from "./components/Events";
+import Conclave from "./components/Conclave";
+import Symposium from "./components/Symposium";
+import TechExpo from "./components/TechExpo";
+import Sponsors from "./components/Sponsors";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
-    <Navbar/>
-    <Routes>
-      <Route path='/' element={<Home/>}/>
-      <Route path='/events' element={<Events/>}/>
-      <Route path='/conclave' element={<Conclave/>}/>
-      <Route path='/symposium' element={<Symposium/>}/>
-      <Route path='/tech-expo' element={<TechExpo/>}/>
-      <Route path='/sponsors' element={<Sponsors/>}/>
-    </Routes>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/events" element={<Events />} />
+        <Route path="/conclave" element={<Conclave />} />
+        <Route path="/symposium" element={<Symposium />} />
+        <Route path="/tech-expo" element={<TechExpo />} />
+        <Route path="/sponsors" element={<Sponsors />} />
+      </Routes>
     </>
   );
 }

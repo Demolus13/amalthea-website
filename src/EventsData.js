96407363ade7825f025e2b6dbecb8eae33155755
Vraj2811 <121240839+Vraj2811@users.noreply.github.com>
2023-09-05 02:05:29 +0530

    Update EventsData.js

--- src/EventsData.js
+++ src/EventsData.js
@@ -3,21 +3,21 @@ export const events = [
     name: "Robowars",
     description:
       "Prepare for an ultimate experience in the ultimate battle of damage, strength, and technology. Witness the head-to-head clash of engineering beasts with metals and electric circuits. To all robo-maniacs, Amalthea 23 presents the most-awaited RoboWars for the first time at IIT Gandhinagar, where all the participants are about Savage Arena, Warrior; Victory is not granted but earned.\nThrilling Robot Combating Event \nIn a fierce battle for supremacy.\nThe robots will face off in an enclosed arena with various obstacles and hazards.",
-    imgSrc: "./images/robowars.png",
+    imgSrc: "./Images/robowars.png",
   },
   {
     name: "coming soon",
     description: "bla bla bla",
-    imgSrc: "./images/coming_soon.png",
+    imgSrc: "./Images/coming_soon.png",
   },
   {
     name: "coming soon",
     description: "bla bla bla",
-    imgSrc: "./images/coming_soon.png",
+    imgSrc: "./Images/coming_soon.png",
   },
   {
     name: "coming soon",
     description: "bla bla bla",
-    imgSrc: "./images/coming_soon.png",
+    imgSrc: "./Images/coming_soon.png",
   },
 ];

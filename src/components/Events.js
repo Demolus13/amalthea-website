a78204b54612b05f4d9c0268b2efc6e6ca900f29
Vivek Raj <VivekRaj27082005@gmail.com>
2023-09-11 00:30:57 +0530

    Adding Video to Events Page

--- src/components/Events.js
+++ src/components/Events.js
@@ -1,33 +1,65 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "../styles/Events.css";
 import { events } from "../utilities/EventsData";
 import { Link } from "react-router-dom";
 
 export default function Events() {
+  const [iframeActive, setIframeActive] = useState(false);
   useEffect(() => {
     const H = window.innerHeight;
     window.scrollTo(0, 0);
-
     const L1 = document.querySelectorAll(".L1");
     const L2 = document.querySelectorAll(".L2");
     const aml = document.getElementById("aml");
     const content = document.getElementById("events-content");
     const eventCards = document.querySelectorAll(".event-card-holder");
+    const event_info = document.getElementsByClassName("events-info");
+    const iframes = document.getElementsByClassName("content-iframe");
+    for (let index = 0; index < iframes.length; index++) {
+      const iframe = iframes[index];
+      iframe.onmouseover = () => {
+        handleHover(0)
+      }
+    }
+    event_info[0].onmouseenter = () => {
+      handleHover(0);
+    };
+
+    event_info[0].onmouseout = () => {
+      handleExit(0);
+    };
+
+    //document.onmo
+    for (let index = 0; index < event_info.length; index++) {
+      const element = event_info[index];
+      element.addEventListener("mouseenter", () => {
+        console.log(index);
+      });
+
+      // element.addEventListener("mouseexit", ()=>{
+      //   handleExit(index)
+      // })
+    }
+    // event_info.forEach((element, index) => {
+    //   element.addEventListener("hover",  () => {
+    //     handleHover(index)
+    //   })
+    // });
 
     const handleScroll = () => {
       const scroll = window.scrollY;
       eventCards[0].style.top = "0vh";
       eventCards[0].style.bottom = "0vh";
 
-      if (scroll < 0.5 * H) {
-        const s1 = 1 - scroll / (0.75 * H);
-        const s2 = 1 - scroll / (0.75 * H);
+      if (scroll < H) {
+        const s1 = 1 - scroll / (0.5 * H);
+        const s2 = 1 - scroll / (0.5 * H);
         const s3 = 1 - scroll / (0.5 * H);
 
-        L1.forEach(l => {
+        L1.forEach((l) => {
           l.style.transform = `scale(${s1})`;
         });
-        L2.forEach(l => {
+        L2.forEach((l) => {
           l.style.transform = `scale(${s2})`;
         });
         aml.style.opacity = s3;
@@ -36,10 +68,15 @@ export default function Events() {
         aml.style.opacity = 0;
         content.style.opacity = 1;
       }
-
+      if (0.55 * H < scroll) {
+        L2.forEach((l) => {
+          l.style.transform = "scale(0)";
+        });
+      }
       if (0.75 * H < scroll) {
         const j = Math.floor((scroll - 0.75 * H) / (2 * H)) + 1;
         const top = (1 - ((scroll - 0.75 * H) % (2 * H)) / (2 * H)) * 100;
+
         if (0 < j && j < 4) {
           eventCards[j].style.top = `${top}vh`;
           eventCards[j].style.bottom = `-${top}vh`;
@@ -60,7 +97,7 @@ export default function Events() {
         }
       }
       if (8.75 * H < scroll) {
-        eventCards.forEach(ec => {
+        eventCards.forEach((ec) => {
           ec.style.top = "0vh";
           ec.style.bottom = "0vh";
         });
@@ -93,8 +130,15 @@ export default function Events() {
         <img className="aml-bg-6" src="./Images/AML-BG-5.png" alt="" />
         <img className="aml-bg-7 L2" src="./Images/AML-BG-6.png" alt="" />
         <div id="events-content">
-          {events.map(event => {
+          {events.map((event, index) => {
+            event["index"] = index;
             return <EventCard key={event.name} {...event} />;
+          })}
+          <h3 className="content-title-0 disable-left">
+            Robowars Video Release
+          </h3>
+          {events.map((event) => {
+            return <EventVisualContent key={event.name} {...event} />;
           })}
         </div>
       </div>
@@ -102,7 +146,26 @@ export default function Events() {
   );
 }
 
-const EventCard = event => {
+const EventVisualContent = (event) => {
+  return (
+    <div className={`contxents-visual`}>
+      {event.contents.map((content, index) => {
+        return (
+            <iframe
+              className={`content-iframe content-iframe-${index} ${
+                index < 4 ? "disable-left" : "disable-right"
+              } contents-for-${event.name}`}
+              height="25%"
+              width="25%"
+              src={content}
+              title="Iframe Example"
+            ></iframe>
+        );
+      })}
+    </div>
+  );
+};
+const EventCard = (event) => {
   const isMobile = window.screen.width < 500 ? true : false;
 
   return (
@@ -117,22 +180,55 @@ const EventCard = event => {
           style={{
             height: event.name === "coming soon" && isMobile ? "95vh" : "",
           }}
-          alt={event.name}></img>
+          alt={event.name}
+        ></img>
         <span
-          className="events-info"
-          style={{ display: event.name === "coming soon" ? "none" : "block" }}>
-          <div className="events-text-holder">
-            <h1 className="oswald">{event.name}</h1>
-            <p className="montserrat">{event.description}</p>
+          className={`events-info events-info-${event._index}`}
+          style={{ display: event.name === "coming soon" ? "none" : "block" }}
+        >
+          <div className="content-placeholder">
+            <div className="content"></div>
+            <div className="events-text-holder">
+              <h1 className="oswald">{event.name}</h1>
+              <p className="montserrat">{event.description}</p>
+            </div>
+            <div className="events-buttons-holder montserrat">
+              <div></div>
+              <Link to={`/register/${event.name.toLowerCase()}`}>
+                <button>Register</button>
+              </Link>
+              <button>Rulebook</button>
+              <div></div>
+            </div>
           </div>
-          <div className="events-buttons-holder montserrat">
-            <Link to={`/register/${event.name.toLowerCase()}`}>
-              <button>Register</button>
-            </Link>
-            <button>Rulebook</button>
-          </div>
+          <div className="content"></div>
         </span>
       </div>
     </div>
   );
+};
+
+const handleHover = (index) => {
+  var handleHoverElements = document.getElementsByClassName(
+    `contents-for-${events[index].name}`
+  );
+  var handleHoverTitle = document.getElementsByClassName(`contents-0-${index}`);
+  for (let index = 0; index < handleHoverElements.length; index++) {
+    const element = handleHoverElements[index];
+    element.classList.remove("disable-left");
+  }
+  document.onmouseover = () => {
+    handleHover(0);
+  };
+};
+
+const handleExit = (index) => {
+  var handleHoverElements = document.getElementsByClassName(
+    `contents-for-${events[index].name}`
+  );
+  for (let index = 0; index < handleHoverElements.length; index++) {
+    const element = handleHoverElements[index];
+    element.classList.add("disable-left");
+  }
+  console.log("Called");
 };
--- src/utilities/EventsData.js
+++ src/utilities/EventsData.js
@@ -4,20 +4,27 @@ export const events = [
     description:
       "Prepare for an ultimate experience in the ultimate battle of damage, strength, and technology. Witness the head-to-head clash of engineering beasts with metals and electric circuits. To all robo-maniacs, Amalthea 23 presents the most-awaited RoboWars for the first time at IIT Gandhinagar, where all the participants are about Savage Arena, Warrior; Victory is not granted but earned.\nThrilling Robot Combating Event \nIn a fierce battle for supremacy.\nThe robots will face off in an enclosed arena with various obstacles and hazards.",
     imgSrc: "./Images/robowars.png",
+    contents: [
+      "Videos/V1.mp4",
+      "Videos/untitled.mp4"
+    ],
   },
   {
     name: "coming soon",
     description: "bla bla bla",
     imgSrc: "./Images/coming_soon.png",
+    contents: [],
   },
   {
     name: "coming soon",
     description: "bla bla bla",
     imgSrc: "./Images/coming_soon.png",
+    contents: [],
   },
   {
     name: "coming soon",
     description: "bla bla bla",
     imgSrc: "./Images/coming_soon.png",
+    contents: [],
   },
 ];
